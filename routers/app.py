bdf3bd081d6577c16143d9f623ad435d6bd3b41b
zaur <zaur.dzasohov@yandex.com>
2025-01-30 20:37:11 +0300

    fix: update endtime to use game.scheduled_datetime instead of g.scheduled_datetime

--- routers/app.py
+++ routers/app.py
@@ -185,7 +185,7 @@ async def read_game(
         "min_ticket_count": game.game.min_ticket_count,
         "max_limit_grid": game.game.max_limit_grid,
         "price": float(game.game.price),
-        "endtime": g.scheduled_datetime.timestamp(),
+        "endtime": game.scheduled_datetime.timestamp(),
         "created": game.created_at.timestamp()
     }
 
